d41c284cc4a80ffa06cec9af34e74d6cf2453e71
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-24 12:09:48 -0300

    titulos en header

--- Frontend/src/components/headerMain/HeaderMain.jsx
+++ Frontend/src/components/headerMain/HeaderMain.jsx
@@ -7,6 +7,7 @@ import { TextMain } from '../textMain/TextMain'
 
 // import { FaArrowCircleDown } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { Col, Row } from 'react-bootstrap'
 
 // const scrollDown = () => {
 //     window.scrollTo(0, 500);
@@ -14,16 +15,21 @@ import { Link } from 'react-router-dom'
 export const HeaderMain = () => {
     return (
         <div className="d-flex flex-column justify-content-start justify-content-lg-center align-items-center header-main">
-            <img className="img-polis-header d-none d-lg-block mt-3" src={headerLogo} alt="header-logo" />
-            {/* <TextMain /> */}
-            <div className="d-none d-md-flex justify-content-center align-items-center px-2 text-center" >
-                <p className='text-main d-flex flex-column m-4  '>
-                    Somos una institución cuyo propósito es fortalecer acciones y lazos comunitarios, la integración, el desarrollo sustentable y el bienestar de la sociedad.
+            {/* <img className="img-polis-header d-none d-lg-block mt-3" src={headerLogo} alt="header-logo" /> */}
+            <Row className="d-none d-md-inline justify-content-center align-items-center px-2 text-center" >
+                <Col xs={6}>   
+                    <h3 className='text-main d-flex flex-column m-4'>
+                        Somos una institución cuyo propósito es fortalecer acciones y lazos comunitarios, la integración, el desarrollo sustentable y el bienestar de la sociedad.
+                    </h3>
+                </Col>
                     <br /> 
-                    Para alcanzar dichos objetivos, nuestros equipos profesionales necesitan de la colaboración de nuestros socixs, colaboradores y suscriptores. Gracias a todos que con sus aportes nos permiten seguir trabajando y ampliando todos los proyectos día a día.
-                    <Link to='/participate' className='text-center text-add'> Sumate a nuestra lista de colaboradores</Link>
-                </p>
-            </div>
+                <Col>   
+                    <h5>
+                        Para alcanzar dichos objetivos, nuestros equipos profesionales necesitan de la colaboración de nuestros socixs, colaboradores y suscriptores. Gracias a todos que con sus aportes nos permiten seguir trabajando y ampliando todos los proyectos día a día.
+                        <Link to='/participate' className='text-center'> Sumate a nuestra lista de colaboradores</Link>
+                    </h5>
+                </Col>
+            </Row>
             <TextMain/>         
             {/* <button onClick={scrollDown} className="d-none d-md-block scroll-down-btn rounded-circle"><FaArrowCircleDown className="mb-1" /></button> */}
         </div>
